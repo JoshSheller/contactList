9b4ced80f748bf2c1c8820b5bcd96387dc499de4
joshsheller <joshua.a.sheller@gmail.com>
2016-03-09 19:59:25 -0800

    Successfuly retrieved data from own mongodb with GET request

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,25 +4,27 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log("Hello world from controller");
 
   $http.get('/contactList').success(function(response) {
-  	console.log("data received from get request!")
+  	console.log("Data received from get request!");
+
+  	$scope.contactList = response;
   });
 
   // var person1 = {
   // 	name: 'Bob',
   // 	email: 'bob@email.com',
-  // 	phone: '(111)-111-1111'
+  // 	number: '(111)-111-1111'
   // };
 
   // var person2 = {
   // 	name: 'Emily',
   // 	email: 'emily@email.com',
-  // 	phone: '(222)-222-2222'
+  // 	number: '(222)-222-2222'
   // };
 
   // var person3 = {
   // 	name: 'George',
   // 	email: 'george@email.com',
-  // 	phone: '(333)-333-3333'
+  // 	number: '(333)-333-3333'
   // };
 
   // var contactList = [person1, person2, person3];
--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 var express = require('express');
 var app = express();
+// require mongo js module
+var mongojs = require('mongojs');
+var db = mongojs('contactList', ['contactList']);
+
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/contactList', function(req, res) {
   console.log('Get request successful!');
 
-  var person1 = {
-  	name: 'Bob',
-  	email: 'bob@email.com',
-  	phone: '(111)-111-1111'
-  };
+  db.contactList.find(function(err, contacts) {
+  	console.log('contacts found -->', contacts);
+  	res.json(contacts);
+  });
 
-  var person2 = {
-  	name: 'Emily',
-  	email: 'emily@email.com',
-  	phone: '(222)-222-2222'
-  };
+  // var person1 = {
+  // 	name: 'Bob',
+  // 	email: 'bob@email.com',
+  // 	number: '(111)-111-1111'
+  // };
 
-  var person3 = {
-  	name: 'George',
-  	email: 'george@email.com',
-  	phone: '(333)-333-3333'
-  };
+  // var person2 = {
+  // 	name: 'Emily',
+  // 	email: 'emily@email.com',
+  // 	number: '(222)-222-2222'
+  // };
 
-  var contactList = [person1, person2, person3];
+  // var person3 = {
+  // 	name: 'George',
+  // 	email: 'george@email.com',
+  // 	number: '(333)-333-3333'
+  // };
+
+  // var contactList = [person1, person2, person3];
 
   // respond to get request with contatList in json object format
-  res.json(contactList);
 });
 
 
